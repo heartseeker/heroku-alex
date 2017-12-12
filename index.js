540fd0a39739069822b67bd0ed9a50020d4e34de
Amistad Alex (ERNI) <amal@erni.ph>
2017-12-12 11:39:19 +0800

    change port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 8000
 
 app.get('/', (req, res) => res.send('Hello Alex World!'))
 
-app.listen(8000, () => console.log('Example app listening on port 8000!'))
+app.listen(PORT, () => console.log('Example app listening on port 8000!'))
